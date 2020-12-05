cd79cdf2185a228bc1c7dea405bffa858326e84a
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-05 22:00:45 +0900

    レビューのcreateメソッドを記述

--- app/controllers/ramen_reviews_controller.rb
+++ app/controllers/ramen_reviews_controller.rb
@@ -1,4 +1,20 @@
 class RamenReviewsController < ApplicationController
   def index
   end
+
+  def create
+    @review = Review.new(ramen_review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to ramen_shop_ramen_reviews_path(@review.ramen_shop)
+    else
+      @ramen_shop = RamenShop.find(params[:id])
+      render "ramen_shops/show"
+    end
+  end
+
+  private
+  def ramen_review_params
+    params.require(:ramen_review).permit(:ramen_shop_id, :score, :content)
+  end
 end
