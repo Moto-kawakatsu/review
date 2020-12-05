c26bd52f304907b37cd522150f09dc3f6b038c57
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-05 22:04:40 +0900

    誤字の修正

--- app/controllers/ramen_reviews_controller.rb
+++ app/controllers/ramen_reviews_controller.rb
@@ -3,7 +3,7 @@ class RamenReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.new(ramen_review_params)
+    @review = RamenReview.new(ramen_review_params)
     @review.user_id = current_user.id
     if @review.save
       redirect_to ramen_shop_ramen_reviews_path(@review.ramen_shop)
