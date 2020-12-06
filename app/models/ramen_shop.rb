0bed0a1063ce12778afbda930bab16ee3931c5ce
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-06 09:37:14 +0900

    レビューの平均スコアとパーセンテージの設定

--- app/models/ramen_shop.rb
+++ app/models/ramen_shop.rb
@@ -1,3 +1,18 @@
 class RamenShop < ApplicationRecord
     has_many :ramen_reviews, dependent: :destroy
+    def avg_score
+        unless self.ramen_reviews.empty?
+            ramen_reviews.average(:score).round(1).to_f
+        else
+            0.0
+        end
+    end
+
+    def ramen_review_score_percentage
+        unless self.ramen_reviews.empty?
+            ramen_reviews.average(:score).round(1).to_f*100/5
+        else
+            0.0
+        end
+    end
 end
