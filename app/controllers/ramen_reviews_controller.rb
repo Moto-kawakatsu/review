08873a74919ccf3ad9b985ed50697ea6c390859b
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-06 09:35:32 +0900

    paramsのrequireを削除

--- app/controllers/ramen_reviews_controller.rb
+++ app/controllers/ramen_reviews_controller.rb
@@ -1,5 +1,8 @@
 class RamenReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
   def index
+    @ramen_shop = RamenShop.find(params[:ramen_shop_id])
+    @ramen_reviews = @ramen_shop.ramen_reviews
   end
 
   def create
@@ -8,13 +11,13 @@ class RamenReviewsController < ApplicationController
     if @review.save
       redirect_to ramen_shop_ramen_reviews_path(@review.ramen_shop)
     else
-      @ramen_shop = RamenShop.find(params[:id])
+      @ramen_shop = RamenShop.find(params[:ramen_shop_id])
       render "ramen_shops/show"
     end
   end
 
   private
   def ramen_review_params
-    params.require(:ramen_review).permit(:ramen_shop_id, :score, :content)
+    params.permit(:ramen_shop_id, :score, :content)
   end
 end
